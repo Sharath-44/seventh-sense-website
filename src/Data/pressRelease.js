b60057fea910af8e4f74ae98765b508a35edcdf1
Sharath-44 <sharath.shashidhara@gmail.com>
2024-07-11 23:46:55 +0530

    press release fix

---
+++ src/Data/pressRelease.js
@@ -0,0 +1,39 @@
+export const videos = [
+    {
+      type: 'youtube',
+      videoId: 'DpG0u4OMe-E',
+      title: "ITC's B-Natural 'Main Bhi Kisaan'",
+      description: "Main Bhi Kisaan (Naanu Kooda Raita), a CSR initiative by ITC, B Natural in collaboration with Seventh Sense and AWAKE to empower and uplift 600+ women fruit farmers. Program Focusing on Financial, Technical,Nutritional literacy and Post production Management.",
+    },
+    {
+      type: 'youtube',
+      videoId: '2BP_6OSjWTA',
+      title: "ITC's B-Natural 'Main Bhi Kisaan'",
+      description: "'Main Bhi Kisaan' project's conclusion event with the Launch of B-Natural Limited Edition 'B-Farmerette' took place in Mumbai on the occasion of Women's Day 2024, which was featured on the CNBC TV18 'Future Female Forward' programme.The farmerettes had a chance to speak about their experience at the event.",
+    },
+    {
+      type: 'youtube',
+      videoId: 'VFslRbCi258',
+      title: "UNICEF - School Innovation Program",
+      description: "The Grand Finale of the School Innovation Program 2022-23 held at Christ University implemented by Seventh Sense in collaboration with Inqui-Lab Foundation.",
+    },
+    {
+      type: 'youtube',
+      videoId: 'BIQBqlFa91g',
+      title: "Youth Co: Lab Summit",
+      description: "Our Managing Director, Vinod Bannanje Prabhu, represented Seventh Sense as the implementing partner for the 5th Edition of Youth Co: Lab at the Youth Co: Lab Summit 2023 in Bangkok: Uniting for Impact through Inclusive Entrepreneurship.",
+    },
+    {
+      type: 'youtube',
+      videoId: 'BrXot0iVfe8',
+      title: "HelloGeleyare",
+      description: 'Our MD, Vinod Bannanje Prabhu, gave an insightful live interview on Doordarshan Chandana, discussing "HelloGeleyare Live Phone In Vruti Darshi: Appropriate Career Guidance for Students." His expert advice aimed to help students navigate their career paths effectively.',
+    },
+    {
+      type: 'local',
+      videoSrc: '/nasscom.mp4', 
+      title: "Local Video Title",
+      description: "Description for the local video.",
+    }
+  ];
+  
