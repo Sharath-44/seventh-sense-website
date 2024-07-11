43c12915d9d6b70bfd1f2ea07708583d768eb64a
Sharath-44 <sharath.shashidhara@gmail.com>
2024-07-12 01:00:08 +0530

    gallery home

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,18 +1,7 @@
 import React from "react";
 import PhotoAlbum from "react-photo-album";
 import '../assets/gallery.css';
-
-const photos = [
-  { src: '/gallery/1.jpg', width: 1400, height: 900, key: '1' },
-  { src: '/gallery/2.jpg', width: 1300, height: 900, key: '2' },
-  { src: '/womenEmpowerment/we19.jpg', width: 1200, height: 900, key: '3' },
-  { src: '/gallery/48.jpg', width: 1400, height: 900, key: '4' },
-  { src: '/galleryImage3.JPG', width: 1400, height: 900, key: '5' },
-  { src: '/gallery/4.jpg', width: 1200, height: 900, key: '6' },
-  { src: '/galleryImage6.jpg', width: 1400, height: 900, key: '7' },
-  { src: '/gallery/7.jpg', width: 1400, height: 1000, key: '8' },
-  { src: '/gallery/5.jpg', width: 1200, height: 1200, key: '9' },
-];
+import {photos} from '../Data/galleryHome';
 
 const Gallery = () => {
   return (
